c582d08f120c14385414035179534be137663aab
KevinKallo <kevin.kallo@tptlive.ee>
2023-10-03 11:25:22 +0300

    Add articles migrate, seeder, factory

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Modules\Article;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        public function run(): void
+    {
+        Article::factory(100)->create();
+        //
+    }
+        //
+    }
+}
